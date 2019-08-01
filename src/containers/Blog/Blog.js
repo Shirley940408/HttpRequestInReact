e1c81ac1480d20ad0d5a9fab88983bae7ad7be7d
Shirley940408 <Shirley940408@gmail.com>
2019-08-01 00:13:34 -0400

    modify details

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,8 +8,10 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null,
     }
+
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
             const posts = response.data.slice(0, 4);//get an new array from the [0] position to [3](4-1) elements
@@ -25,9 +27,13 @@ class Blog extends Component {
         })
         //axios used promises to achieve asychronize
     }
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>;
+            return <Post key = {post.id} title = {post.title} author = {post.author} clicked = {() => this.postSelectedHandler(post.id)} />;
         });
         return (
             <div>
@@ -35,7 +41,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id = {this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
