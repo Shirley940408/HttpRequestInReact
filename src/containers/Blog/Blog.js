76db5770c35436821f311c0c67f8b63cc8bbd130
Shirley940408 <Shirley940408@gmail.com>
2019-07-31 22:13:16 -0400

    using axios to fetch data

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+
+class Blog extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            const posts = response.data.slice(0, 4);//get an new array from the [0] position to [3](4-1) elements
+            const updatedPosts = posts.map(post => {
+                return{
+                    ...post,
+                    author: 'Max'
+                }
+            });
+
+            this.setState({ posts: updatedPosts});
+            // console.log(response);
+        })
+        //axios used promises to achieve asychronize
+    }
+    render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author}/>;
+        });
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
