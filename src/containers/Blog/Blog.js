5b8ae1056a93a4897b7dea739f8d2c0460280e1b
Shirley940408 <Shirley940408@gmail.com>
2019-08-13 03:59:21 -0400

    guard auth and 404 case

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,14 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import NewPost from './Posts/NewPost/NewPost';
 import FullPost from './Posts/FullPost/FullPost';
 class Blog extends Component {
+    
+    state = {
+        auth: false,
+    }
+
+    componentDidMount() {
+        console.log(this.props);
+    }
 
     render () {
         return (
@@ -32,9 +40,10 @@ class Blog extends Component {
                 {/*<Route path = "/" exact render = {() => <h1>Home</h1>}/>
                     <Route path = "/" render = {() => <h1>Home2</h1>}/>*/}
                 <Switch>
-                    <Route path = "/new-post" component = {NewPost}/>
+                    {this.state.auth? <Route path = "/new-post" component = {NewPost}/>: null} 
                     <Route path = "/posts" component = {Posts}/>
-                    <Redirect from = "/" to = "/posts" />
+                    <Route render = {() => <h1>Not found</h1>}/>
+                    {/**<Redirect from = "/" to = "/posts" /> */} 
                 </Switch>
             </div>
         );
